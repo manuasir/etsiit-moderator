31e9d0a606157a1aa3a4e68bb17ba8f911afb927
Adolfo Martinez <adolfomartinez007@gmail.com>
2017-07-23 21:01:50 +0200

    Correcciones y comandos horario y horarios
    Se han realizado correcciones en el codigo.
    
    Se ha aÃadido el comando horarios. Al utilizar este comando el bot
    muestra la url de los horarios para los diferentes cursos de las
    titulaciones de la ETSIIT.
    
    Se ha añadido el comando horario. Al utilizar este comando se muestra
    el horario de un servicio prestado en la ETSIIT, por ejemplo secretaria,
    biblioteca, comedores, etc. Incluye una opción para listar todos los
    servicios.

--- util/urls.js
+++ util/urls.js
@@ -1,5 +1,6 @@
 module.exports = {
   normativa: 'http://secretariageneral.ugr.es/bougr/pages/bougr112/_doc/examenes%21',
   examenes: 'http://deiit.ugr.es/BDExamenes/search.html',
-  delegacion: 'http://deiit.ugr.es'
+  delegacion: 'http://deiit.ugr.es',
+  horarios: 'No disponible. Disculpa las molestias.'
 };
