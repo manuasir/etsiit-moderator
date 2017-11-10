bf9e7fea0185e2268f6a80162e454b6aaa304ea0
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2017-11-10 09:22:58 +0100

    Múltiples mejoras

--- util/servicios.js
+++ util/servicios.js
@@ -1,8 +1,8 @@
 module.exports = {
-    etsiit    : 'El horario de funcionamiento de la ETSIIT es de 8:00 a 22:00',
-    secretaria: 'El horario de secretaría es de 9:00 a 14:00',
-    copisteria: 'El horario de copistería es de 9:00 a 14:00 y de 16:00 a 20:00',
-    comedores : 'El horario de los comedores universitarios es de 13:00 a 15:30',
-    cafeteria : 'El horario de la cafetería es de 8:00 a 20:00',
-    biblioteca: 'El horario de la biblioteca es de 8:30 a 20:30'
+    etsiit    : '👨‍💻 El horario de funcionamiento de la ETSIIT es de 8:00 a 22:00',
+    secretaria: '💻 El horario de secretaría es de 9:00 a 14:00',
+    copisteria: '🖨 El horario de copistería es de 9:00 a 14:00 y de 16:00 a 20:00',
+    comedores : '🍽 El horario de los comedores universitarios es de 13:00 a 15:30',
+    cafeteria : '☕️ El horario de la cafetería es de 8:00 a 20:00',
+    biblioteca: '📖 El horario de la biblioteca es de 8:30 a 20:30'
 };
